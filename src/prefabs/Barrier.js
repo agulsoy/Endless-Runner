8e47a74fa8246d0615da4fc5be0207b5b140c5d7
pbsulliv <pbsulliv@ucsc.edu>
2020-04-29 16:30:56 -0700

    barriers spawn at bottom

--- src/prefabs/Barrier.js
+++ src/prefabs/Barrier.js
@@ -1,11 +1,11 @@
 class Barrier extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, velocity) {
         // call Phaser Physics Sprite constructor
-        super(scene, game.config.width + paddleWidth, Phaser.Math.Between(paddleHeight/2, game.config.height - paddleHeight/2), 'platform'); 
+        super(scene, Phaser.Math.Between(paddleHeight/2, game.config.height - paddleHeight/2), game.config.width + paddleWidth, 'platform'); 
         // set up physics sprite
         scene.add.existing(this);               // add to existing scene, displayList, updateList
         scene.physics.add.existing(this);       // add physics body
-        this.setVelocityX(velocity);            // make it go!
+        this.setVelocityY(velocity);            // make it go!
         this.setImmovable();                    
         this.tint = Math.random() * 0xFFFFFF;   // randomize tint
         this.newBarrier = true;                 // custom property to control barrier spawning
@@ -19,14 +19,14 @@ class Barrier extends Phaser.Physics.Arcade.Sprite {
         super.update();
 
         // add new barrier when existing barrier hits center X
-        if(this.newBarrier && this.x < centerX) {
+        if(this.newBarrier && this.y < centerY) {
             this.newBarrier = false;
             // call parent scene method from this context
             this.scene.addBarrier(this.parent, this.velocity);
         }
 
         // destroy paddle if it reaches the left edge of the screen
-        if(this.x < -this.width) {
+        if(this.y < -this.width) {
             this.destroy();
         }
     }
