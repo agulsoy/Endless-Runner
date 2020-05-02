3c332728d2da9e6cd897958c16efa086cc5e4ad5
pbsulliv <pbsulliv@ucsc.edu>
2020-05-01 17:29:24 -0700

    Added basic jumping

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,6 +15,11 @@ class Play extends Phaser.Scene {
          level = 0;
          this.extremeMODE = false;
          this.shadowLock = false;
+
+         //set up world physics
+         this.physics.world.gravity.y = 2000;
+         this.JUMP_VELOCITY = -2000;
+         this.hasJumped = false;
  
          // set up cursor keys
          cursors = this.input.keyboard.createCursorKeys();
@@ -24,11 +29,12 @@ class Play extends Phaser.Scene {
          paddle.setCollideWorldBounds(true);
          paddle.setBounce(0.5);
          paddle.setImmovable();
-         paddle.setMaxVelocity(600, 0);
+         paddle.setMaxVelocity(600, 600);
          paddle.setDragX(200);
          paddle.setDepth(1);         // ensures that paddle z-depth remains above shadow paddles
          paddle.destroyed = false;   // custom property to track paddle life
  
+         /*
          // set up barrier group and add first barrier to kick things off
          this.barrierGroup = this.add.group({
              runChildUpdate: true    // make sure update runs on group children
@@ -42,6 +48,7 @@ class Play extends Phaser.Scene {
              callbackScope: this,
              loop: true
          });
+         */
      }
  
      addBarrier() {
@@ -49,6 +56,7 @@ class Play extends Phaser.Scene {
          this.barrierGroup.add(barrier);                         // add it to existing group
      }
  
+     
      update() {
          if(!paddle.destroyed) {
              // check for player input
@@ -57,6 +65,18 @@ class Play extends Phaser.Scene {
              } else if(cursors.right.isDown) {
                  paddle.body.velocity.x += paddleVelocity;
              }
+
+             // check for jump input
+             if(Phaser.Input.Keyboard.DownDuration(cursors.up, 150) && (this.hasJumped === false)) {
+                 paddle.body.velocity.y = this.JUMP_VELOCITY;
+                 this.hasJumped = true;
+             }
+
+             //if paddle sprite is changed remember to check hardcoded value paddleWidth!
+             if(paddle.y > ((centerY * 2) - (33))) {
+                this.hasJumped = false;
+             }
+
              // check for collisions
              this.physics.world.collide(paddle, this.barrierGroup, this.paddleCollision, null, this);
          }
