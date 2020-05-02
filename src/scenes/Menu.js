44fb4f5f990f2ed8dad4a07d78a101f196b2cf43
agulsoy <agulsoy@ucsc.edu>
2020-05-02 13:47:21 -0700

    MenuScene simplification

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,9 +4,29 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
+        //menu configuration
+        let menuConfig = {
+            fontFamily: 'Helvetica',
+            fontSize: '48px',
+            backgroundColor: '#000000',
+            color: '#33A2FF',
+            align: 'center',
+            padding: {
+                top:5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
         // add menu screen text
-        this.add.text(centerX, (centerY - (centerY/2)), 'Endless Climber', { fontFamily: 'Helvetica', fontSize: '48px', color: '#33A2FF' }).setOrigin(0.5);
-        this.add.text(centerX, centerY + (centerY/2), 'Press SPACEBAR to Start', { fontFamily: 'Helvetica', fontSize: '24px', color: '#33E6FF' }).setOrigin(0.5);
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+        let textSpacer = 64;
+
+        this.add.text(centerX, (centerY - (centerY/2)), 'Endless Climber', menuConfig).setOrigin(0.5);
+        menuConfig.fontSize = '24px';
+        menuConfig.color = '#33E6FF';
+        this.add.text(centerX, centerY + (centerY/2), 'Press SPACEBAR to Start', menuConfig).setOrigin(0.5);
 
         // define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
