50c3bad83db34ef94d3259e295a7ff2a3187206c
pbsulliv <pbsulliv@ucsc.edu>
2020-05-03 20:00:02 -0700

    cleaned up code

--- src/prefabs/Barrier.js
+++ src/prefabs/Barrier.js
@@ -3,18 +3,16 @@
 
 class Barrier extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, velocity) {
-        // call Phaser Physics Sprite constructor
-        //super(scene, Phaser.Math.Between(paddleHeight/2, game.config.height - paddleHeight/2), 0, 'platform');  
+        // call Phaser Physics Sprite constructor 
         super(scene, Phaser.Math.Between(0, game.config.width - paddleWidth), 0, 'platform');  
 
         // set up physics sprite
         scene.add.existing(this);               // add to existing scene, displayList, updateList
         scene.physics.add.existing(this);       // add physics body
         this.body.setAllowGravity(false);
-        this.setVelocityY(-1 * velocity);            // make it go!
+        this.setVelocityY(-1 * velocity);            // set y velocity
         this.setImmovable();                    
-        this.tint = Math.random() * 0xFFFFFF;   // randomize tint
-        this.newBarrier = true;                 // custom property to control barrier spawning
+        this.newBarrier = true;                 
 
         this.scene = scene;
         this.velocity = velocity;
@@ -24,14 +22,14 @@ class Barrier extends Phaser.Physics.Arcade.Sprite {
         // override physics sprite update()
         super.update();
 
-        // add new barrier when existing barrier hits center Y
+        // add new barrier when existing barrier hits center Y/2
         if(this.newBarrier && this.y > (centerY/2)) {
             this.newBarrier = false;
             // call parent scene method from this context
             this.scene.addBarrier(this.parent, this.velocity);
         }
 
-        // destroy paddle if it reaches the left edge of the screen
+        // destroy paddle if it reaches the bottom edge of the screen
         if(this.y > (game.config.height)) {
             this.destroy();
         }
